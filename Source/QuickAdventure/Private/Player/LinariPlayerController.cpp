567b7e39abd302a3773c5af1f389a95dee3b6f47
Dawid Harendarz <dharendarz2@gmail.com>
2024-03-30 07:50:43 +0100

    add actions for character like, moving, looking and jumping

--- Source/QuickAdventure/Private/Player/LinariPlayerController.cpp
+++ Source/QuickAdventure/Private/Player/LinariPlayerController.cpp
@@ -2,4 +2,62 @@
 
 
 #include "Player/LinariPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Character/LinariCharacter.h"
+
+void ALinariPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+}
+
+void ALinariPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ALinariPlayerController::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ALinariPlayerController::Look);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ALinariPlayerController::Jump);
+	
+}
+
+void ALinariPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection,InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
+	}
+}
+
+void ALinariPlayerController::Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookVector = InputActionValue.Get<FVector2D>();
+	AddPitchInput(LookVector.Y);
+	AddYawInput(LookVector.X);
+}
+
+void ALinariPlayerController::Jump(const FInputActionValue& InputActionValue)
+{
+	if (GetCharacter())
+	{
+		GetCharacter()->Jump();
+	}
+	
+}
 
--- Source/QuickAdventure/Public/Player/LinariPlayerController.h
+++ Source/QuickAdventure/Public/Player/LinariPlayerController.h
@@ -1,6 +1,12 @@
 // Copyright Dawid Harendarz.
 
 #pragma once
+#include "InputActionValue.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+struct FInputActionValue;
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
@@ -13,5 +19,26 @@ UCLASS()
 class QUICKADVENTURE_API ALinariPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	void Move(const FInputActionValue& InputActionValue);
+	void Look(const FInputActionValue& InputActionValue);
+	void Jump(const FInputActionValue& InputActionValue);
 };
