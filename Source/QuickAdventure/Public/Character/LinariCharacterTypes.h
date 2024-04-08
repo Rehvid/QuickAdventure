16fa194e2acf004f78b5d452a555f5ddd4ef7232
Dawid Harendarz <dharendarz2@gmail.com>
2024-04-08 06:45:21 +0200

    Add new character states

--- Source/QuickAdventure/Public/Character/LinariCharacterTypes.h
+++ Source/QuickAdventure/Public/Character/LinariCharacterTypes.h
@@ -5,5 +5,7 @@ enum class ELinariCharacterState : uint8
 {
 	ELCS_Unoccupied UMETA(DisplayName = "Unoccupied"),
 	ELCS_Walking UMETA(DisplayName = "Walking"),
-	ELCS_Running UMETA(DisplayName = "Running")
+	ELCS_Running UMETA(DisplayName = "Running"),
+	ELCS_Crouch UMETA(DisplayName = "Crouching"),
+	ELCS_Jumping UMETA(DisplayName = "Jumping")
 };
